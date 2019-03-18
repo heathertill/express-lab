5d9f9685270f41910239c5df7795743710cd98d2
Heather Till <heather.till@charter.net>
2019-03-18 18:01:16 -0500

    working on making form functional

--- public/js/app.js
+++ public/js/app.js
@@ -1,3 +1,16 @@
-// alert('wow');
+$.ajax({
+    method: 'GET',
+    url: '/films'
+})
+    .then(films => {
+    
+        films.forEach(film => {
+            $('.list-group').append(`
+            <li class="list-group-item">${film.name} likes <i>${film.filmTitle}</i></li>
+    `)
+            
+})
+
+})
 
 
--- server/server.js
+++ server/server.js
@@ -1,26 +1,40 @@
 const express = require('express');
 const path = require('path');
-const bodyParser = require('body-parser');
+const fs = require('fs');
+
+
 let app = express();
-
-
-
 
 app.use((req, res, next) => {
     console.log(req.originalUrl);
     next();
-})
-
-
-
-
-
-app.get('/', (req, res) => {
-    res.send('Hello from the web server side...')
 });
 
+app.use(express.static('public'));
 
-app.use(express.static(path.join(__dirname, '../public')));
+app.get('/films', (req, res, next) => {
+    fs.readFile(path.join(__dirname, './data/films.json'), (err, data) => {
+        if (err) {
+            console.log('Error')
+            console.log(err);
+            res.sendStatus(500);
+        }
+       
+        let films = JSON.parse(data)
+       
+        res.json(films);
+
+   })
+    
+});
+
+// app.get('/', (req, res) => {
+//     res.send('Hello from the web server side...');
+    
+// });
+
 
 
 app.listen(3000);
+
+
